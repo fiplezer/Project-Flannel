ecf2b1ab7078e4ba21479557bdd06c29a3020602
fiplezer <jessebraaksma12@gmail.com>
2025-11-10 15:34:32 +0100

    mogelijke input-system fix

---
+++ Project Flannel/Assets/scripts/MoveCamera.cs
@@ -0,0 +1,12 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public Transform cameraPosition;
+
+    private void Update()
+    {
+        transform.position = cameraPosition.position;
+    }
+}
